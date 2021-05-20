6b27c3a3c0c5ab77458a50ededae1c3b5c2f2257
Thamme Gowda <tgowdan@gmail.com>
2021-05-20 16:40:29 -0700

    add script to extract validation stats

---
+++ runs/report-val.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+exp=$1
+metric=$2
+if [[ "$#" -ne 2 ]]; then
+    echo "Usage: <exp/dir> <Accuracy|MacroF1|MicroF1>"
+    exit 2;
+fi 
+
+grep $metric $(
+    ls $exp/models/validation-* |
+        while read i; do
+            echo $(echo $i | grep -o '[0-9]*000') $i
+        done |
+        sort -n | cut -f2 -d' '
+     ) | sed 's:/models/validation-:\t:;s/.txt:/\t/'
