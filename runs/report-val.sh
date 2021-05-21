48e900fa708f85f82b7f2b8bcdfe0bb54557b03b
Thamme Gowda <tg@isi.edu>
2021-05-20 17:38:42 -0700

    robust parsing of step number

--- runs/report-val.sh
+++ runs/report-val.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 exp=$1
 metric=$2
+
 if [[ "$#" -ne 2 ]]; then
     echo "Usage: <exp/dir> <Accuracy|MacroF1|MicroF1>"
     exit 2;
 fi 
 
-grep $metric $(
-    ls $exp/models/validation-* |
+files=$(ls $exp/models/validation-* |
         while read i; do
-            echo $(echo $i | grep -o '[0-9]*000') $i
+            echo $(echo $i | grep -o '[0-9]*000.txt' | sed 's/\.txt//') $i
         done |
-        sort -n | cut -f2 -d' '
-     ) | sed 's:/models/validation-:\t:;s/.txt:/\t/'
+        sort -n |
+	cut -f2 -d' '
+     )
+#echo $files
+grep $metric $files | sed 's:/models/validation-:\t:;s/.txt:/\t/'
